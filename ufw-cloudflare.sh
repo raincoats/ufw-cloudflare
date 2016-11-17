c1c814c8a17533616e8ac2a89a7622816c793d95
raincoats <raincoats@users.noreply.github.com>
2016-11-17 14:59:48 +1100

    more options, added a remove mode

--- ufw-cloudflare.sh
+++ ufw-cloudflare.sh
@@ -13,11 +13,34 @@
 
 if [[ $1 =~ ^-*h(elp)?$ ]]; then
 	printf -- "%s: ufw rule generator for CloudFlare\n" $0
-	printf -- "usage: %s | sudo sh\n" $0
+	printf -- "usage:\n"
+	printf -- "          to add the rules:  %s\n" $0
+	printf -- "  to see what would happen:  %s test\n" $0
+	printf -- "           to delete rules:  %s rm\n" $0
+	printf -- "\nhttps://github.com/raincoats/ufw-cloudflare\n"
 	exit 0
+elif [[ $1 =~ ^-*rm$ ]]; then
+	while true; do
+		r=$(ufw status numbered \
+			| egrep '# CloudFlare$' \
+			| tr -d '[]' \
+			| awk '{ print $1 }')
+		[[ -z $r ]] && break
+		yes | ufw delete $r
+	done
+	exit 0
+elif [[ $1 =~ ^-*test$ ]]; then
+	shell=cat
+fi
+
+
+if [[ $(id -u) -ne 0 ]] && [[ -z $shell ]]; then
+	printf "you will need to run this as root, sorry :/\n"
+	exit 1
 fi
 
 curl -s https://www.cloudflare.com/ips-v{4,6} \
-| sed 's/.*/ufw allow from & to any port 80,443 proto tcp comment CloudFlare/'
+| sed 's/.*/ufw allow from & to any port 80,443 proto tcp comment CloudFlare/' \
+| ${shell:-sh}
 
 exit 0
